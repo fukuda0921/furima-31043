91f62b126ab47eba4416acc5f4222af0e16a5f3a
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-05 15:41:56 +0900

    重複の解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
-
+  
   with_options presence: true do
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :nickname
+    validates :birth_date
   end
 end
