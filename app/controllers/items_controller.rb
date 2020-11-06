38bafc83dc766a700ace27a70278670479963b15
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-06 11:11:19 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
+    
   end
 
   def new
@@ -7,6 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item =
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:explanation,:category_id,:status_id,:delivery_fee_id,:prefecture_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,14 +8,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
-    with_options presence: true do
-      validates :name
-      validates :explanation
-      validates :price
-    end
-    validates :category, numericality: { other_than: 1 }
-    validates :status_id, numericality: { other_than: 1 }
-    validates :delivery_fee_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 0 }
-    validates :days_to_ship_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :price
+  end
+  validates :price, numericality: {only_integer: true, with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price, inclusion: {in: 300..9999999, message: 'Out of setting range'}
+    validates :category_id, numericality: { other_than: 1 , message: 'Select'}
+    validates :status_id, numericality: { other_than: 1 , message:'Select'}
+    validates :delivery_fee_id, numericality: { other_than: 1 , message:'Select'}
+    validates :prefecture_id, numericality: { other_than: 0 , message:'Select'}
+    validates :days_to_ship_id, numericality: { other_than: 1 , message:'Select'}
 end
