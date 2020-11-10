2e177786d2df712b088b57e8d87117fbd07d609a
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-10 20:50:09 +0900

    環境変数の設定

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -23,7 +23,7 @@ class PurchaseController < ApplicationController
   end
   
   def pay_item
-    Payjp.api_key = "sk_test_74e81a748fb91d20e308bf49"  
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_address_params[:token],
