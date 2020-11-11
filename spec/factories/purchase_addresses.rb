541dedc8801b4d070cc5b856eb4972b619770992
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-11 11:29:55 +0900

    環境変数の修正
    This reverts commit 2988b528a70426cd6721adaa4d2332b19d0b4c6e.

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :purchase_address do
+    token {"tok_abcdefghijk00000000000000000"}
     postal_code { '756-4446'}
     municipality { '福岡市博多区'}
     street_number { '5−4'}
